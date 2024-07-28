2e9d06eee26cfe9e26226a5401818f1996565a8b
Mohammad Ansil :) <146548918+MasterOfBrokenLogic@users.noreply.github.com>
2024-07-28 15:47:42 +0530

    Fix async/await issue in IPReset

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const IPReset = require('./src');
-const ipr = new IPReset('platform-tools\adb.exe',1000);
-(() => {
-	await ipr.reset();
-	console.log('Your IP Address has been changed!')
+const ipr = new IPReset('platform-tools\\adb.exe', 1000);
+
+(async () => {
+    try {
+        await ipr.reset();
+        console.log('Your IP Address has been changed!');
+    } catch (error) {
+        console.error('Failed to change IP address:', error);
+    }
 })();
